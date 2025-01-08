fee3f45c221ca25af6dad4549c129e6a3aed6bd3
danny <weilong712@gmail.com>
2025-01-08 23:36:02 +0800

    placing enemies and kicking them

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import copy
 import tcod
 
 from engine import Engine
 from input_handlers import EventHandler
-from entity import Entity
+import entity_factories
 from procgen import generate_dungeon
 
 def main() -> None:
@@ -14,17 +15,16 @@ def main() -> None:
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
+    max_monsters_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
-    entities = {npc, player}
-    game_map = generate_dungeon(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
-    engine = Engine(entities=entities, event_handler=event_handler, player=player, game_map=game_map)
+    player = copy.deepcopy(entity_factories.player)
+    game_map = generate_dungeon(max_rooms, room_min_size, room_max_size, map_width, map_height, player, max_monsters_per_room)
+    engine = Engine(event_handler=event_handler, player=player, game_map=game_map)
 
     with tcod.context.new_terminal(
         screen_width,
