1ac88cfc6e52b90cf453caf7654db74cec914e7a
Danny <weilong712@gmail.com>
2025-01-10 11:31:38 +0800

    feat: Doing some damage

--- main.py
+++ main.py
@@ -21,10 +21,18 @@ def main() -> None:
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
     player = copy.deepcopy(entity_factories.player)
-    game_map = generate_dungeon(max_rooms, room_min_size, room_max_size, map_width, map_height, player, max_monsters_per_room)
-    engine = Engine(event_handler=event_handler, player=player, game_map=game_map)
+    engine = Engine(player=player)
+    engine.game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        max_monsters_per_room=max_monsters_per_room,
+        engine=engine,
+    )
+    engine.update_fov()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -36,7 +44,7 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             engine.render(root_console, context)
-            engine.handle_events(tcod.event.wait())
+            engine.event_handler.handle_events()
 
 if __name__ == "__main__":
     main()
